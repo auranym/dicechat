b96896e8d6f6181d4a24be0ab9a09c145615cfc6
auranym <auranym.media@gmail.com>
2024-09-26 16:44:28 -0700

    created first component to reference for future components

---
+++ src/components/message.js
@@ -0,0 +1,35 @@
+export class Message extends HTMLElement {
+  static observedAttributes = ["user", "message"];
+  shadowRoot;
+
+  constructor() {
+    super();
+    this.user = "??";
+    this.message = "";
+    this.shadowRoot = this.attachShadow({ mode: "open" });
+    this.render();
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this[name] = newValue;
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+      <p>
+        <span>${this.user}:</span> ${this.message}
+      </p>
+      <style>
+        p {
+          margin: 0;
+        }
+        span {
+          font-weight: var(--font-weight-bold)
+        }
+      </style>
+    `;
+  }
+}
+
+customElements.define("dc-message", Message);
